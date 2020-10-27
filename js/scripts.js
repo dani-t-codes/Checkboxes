41b00e236ed8dfa1ce691f8349208af7191de42d
David Sterry <sterry.david@gmail.com>
2020-10-27 14:11:20 -0700

    add to Uppercase for work transportation
    Co-authored-by: Danielle Thompson <71739178+dani-t-codes@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
   $("form#transportation_survey").submit(function(event) {
     event.preventDefault();
     $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function() {
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
       const workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+      const capitalWorkTransportationMode = workTransportationMode.toUpperCase();
+      $('#work-responses').append(capitalWorkTransportationMode + "<br>");
     });
     $("#fun-responses").show();
     $("input:checkbox[name=fun-transportation]:checked").each(function(){
